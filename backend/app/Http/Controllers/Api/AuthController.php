5ef9c578fa8d64b10249cbc09fb469770202421d
Ivan Savon <isavon.we@gmail.com>
2024-08-11 09:08:38 +0300

    make signup and testing

---
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\SignupRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function signup(SignupRequest $request)
+    {
+        $data = $request->validated();
+
+        /** @var User $user */
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response(compact('user', 'token'));
+    }
+}
---
+++ backend/tests/Feature/AuthTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_signup_wrong_data()
+    {
+        $response = $this->json('POST', '/api/signup', [
+            'name' => 'name',
+            'password' => 'q11',
+            'password_confirmation' => 'q112',
+        ]);
+
+        $response->assertStatus(422);
+    }
+
+    public function test_signup_ok()
+    {
+        $response = $this->json('POST', '/api/signup', [
+            'name' => 'name',
+            'email' => 'test@test.com',
+            'password' => 'q11',
+            'password_confirmation' => 'q11',
+        ]);
+
+        $response->assertStatus(200);
+    }
+
+    public function test_signup_exists_email()
+    {
+        $response = $this->json('POST', '/api/signup', [
+            'name' => 'test',
+            'email' => 'test@test.com',
+            'password' => 'q11',
+            'password_confirmation' => 'q11',
+        ]);
+
+        $response->assertStatus(422);
+    }
+}
